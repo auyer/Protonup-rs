6696f77a15aef2a9fcb88c42c6a253a682ddf71b
Auyer <rafael@rcpassos.me>
2022-09-03 18:24:18 -0300

    Breaking into submodules, custom install location

--- src/github.rs
+++ src/github.rs
@@ -1,10 +1,8 @@
-const GITHUB_REPO: &str = "proton-ge-custom";
-const GITHUB_ACCOUNT: &str = "GloriousEggroll";
+use super::constants;
 
 #[derive(Default, Debug, PartialEq)]
 pub struct Download {
     pub version: String,
-    pub date: String,
     pub sha512sum: String,
     pub download: String,
     pub size: u64,
@@ -12,31 +10,30 @@ pub struct Download {
 
 pub async fn list_releases() -> Result<Vec<octocrab::models::repos::Release>, octocrab::Error> {
     let releases = octocrab::instance()
-        .repos(GITHUB_ACCOUNT, GITHUB_REPO)
+        .repos(constants::GITHUB_ACCOUNT, constants::GITHUB_REPO)
         .releases()
         .list()
-        .per_page(10)
+        .per_page(30)
         .page(1u32)
         .send()
-        // .get_latest()
         .await?
         .take_items();
     Ok(releases)
 }
 
-pub async fn fetch_data(tag: &str) -> Result<Download, octocrab::Error> {
+pub async fn fetch_data_from_tag(tag: &str) -> Result<Download, octocrab::Error> {
     let mut download = Download::default();
     let release = match tag {
         "latest" => {
             octocrab::instance()
-                .repos(GITHUB_ACCOUNT, GITHUB_REPO)
+                .repos(constants::GITHUB_ACCOUNT, constants::GITHUB_REPO)
                 .releases()
                 .get_latest()
                 .await?
         }
         _ => {
             octocrab::instance()
-                .repos(GITHUB_ACCOUNT, GITHUB_REPO)
+                .repos(constants::GITHUB_ACCOUNT, constants::GITHUB_REPO)
                 .releases()
                 .get_by_tag(tag)
                 .await?
@@ -44,7 +41,6 @@ pub async fn fetch_data(tag: &str) -> Result<Download, octocrab::Error> {
     };
 
     download.version = release.tag_name;
-    // download.date = release.published_at;
     for ass in &release.assets {
         if ass.name.ends_with("sha512sum") {
             download.sha512sum = ass.browser_download_url.as_str().to_string();
---
+++ src/utils.rs
@@ -0,0 +1,21 @@
+use std::path::{Path, PathBuf};
+
+pub fn expand_tilde<P: AsRef<Path>>(path_user_input: P) -> Option<PathBuf> {
+    let p = path_user_input.as_ref();
+    if !p.starts_with("~") {
+        return Some(p.to_path_buf());
+    }
+    if p == Path::new("~") {
+        return dirs::home_dir();
+    }
+    dirs::home_dir().map(|mut h| {
+        if h == Path::new("/") {
+            // Corner case: `h` root directory;
+            // don't prepend extra `/`, just drop the tilde.
+            p.strip_prefix("~").unwrap().to_path_buf()
+        } else {
+            h.push(p.strip_prefix("~/").unwrap());
+            h
+        }
+    })
+}
