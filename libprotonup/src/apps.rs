363759481c1948d226525356d0f907b5bead6c94
Stuart Reilly <stu@reilly-family.co.uk>
2024-03-10 13:16:32 +0000

    Use more async and allow parallel downloads

--- libprotonup/src/apps.rs
+++ libprotonup/src/apps.rs
@@ -1,8 +1,11 @@
+use arcstr::ArcStr;
+use futures_util::stream::{self, StreamExt};
+use std::fmt;
+
 use crate::{
     files::{self, list_folders_in_path},
     variants::Variant,
 };
-use std::fmt;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum App {
@@ -41,13 +44,15 @@ impl App {
     }
 
     /// Checks the versions (Native vs Flatpak) of the App that are installed
-    pub fn detect_installation_method(&self) -> Vec<AppInstallations> {
+    pub async fn detect_installation_method(&self) -> Vec<AppInstallations> {
         match *self {
             Self::Steam => {
                 detect_installations(&[AppInstallations::Steam, AppInstallations::SteamFlatpak])
+                    .await
             }
             Self::Lutris => {
                 detect_installations(&[AppInstallations::Lutris, AppInstallations::LutrisFlatpak])
+                    .await
             }
         }
     }
@@ -74,19 +79,23 @@ impl fmt::Display for AppInstallations {
 
 impl AppInstallations {
     /// Default directory that wine is extracted to
-    pub fn default_install_dir(&self) -> &'static str {
+    pub const fn default_install_dir(&self) -> ArcStr {
         match *self {
-            Self::Steam => "~/.steam/steam/compatibilitytools.d/",
+            Self::Steam => arcstr::literal!("~/.steam/steam/compatibilitytools.d/"),
             Self::SteamFlatpak => {
-                "~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/"
+                arcstr::literal!(
+                    "~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/"
+                )
             }
-            Self::Lutris => "~/.local/share/lutris/runners/wine/",
-            Self::LutrisFlatpak => "~/.var/app/net.lutris.Lutris/data/lutris/runners/wine/",
+            Self::Lutris => arcstr::literal!("~/.local/share/lutris/runners/wine/"),
+            Self::LutrisFlatpak => {
+                arcstr::literal!("~/.var/app/net.lutris.Lutris/data/lutris/runners/wine/")
+            }
         }
     }
 
     /// The app root folder
-    pub fn app_base_dir(&self) -> &'static str {
+    pub const fn app_base_dir(&self) -> &'static str {
         match *self {
             Self::Steam => "~/.steam/steam/",
             Self::SteamFlatpak => "~/.var/app/com.valvesoftware.Steam/data/Steam/",
@@ -96,12 +105,12 @@ impl AppInstallations {
     }
 
     /// Get a list of the currently installed wine versions
-    pub fn list_installed_versions(&self) -> Result<Vec<String>, anyhow::Error> {
-        list_folders_in_path(self.default_install_dir())
+    pub async fn list_installed_versions(&self) -> Result<Vec<String>, anyhow::Error> {
+        list_folders_in_path(&self.default_install_dir()).await
     }
 
     /// Returns the base App
-    pub fn into_app(&self) -> App {
+    pub const fn as_app(&self) -> App {
         match *self {
             Self::Steam | Self::SteamFlatpak => App::Steam,
             Self::Lutris | Self::LutrisFlatpak => App::Lutris,
@@ -110,17 +119,23 @@ impl AppInstallations {
 }
 
 /// list_installed_apps returns a vector of App variants that are installed
-pub fn list_installed_apps() -> Vec<AppInstallations> {
-    detect_installations(APP_INSTALLATIONS_VARIANTS)
+pub async fn list_installed_apps() -> Vec<AppInstallations> {
+    detect_installations(APP_INSTALLATIONS_VARIANTS).await
 }
 
 /// detect_installations returns a vector of App variants that are detected
-fn detect_installations(app_installations: &[AppInstallations]) -> Vec<AppInstallations> {
-    app_installations
-        .iter()
-        .filter(|app| files::check_if_exists(app.app_base_dir(), ""))
-        .cloned()
+async fn detect_installations(app_installations: &[AppInstallations]) -> Vec<AppInstallations> {
+    stream::iter(app_installations)
+        .filter_map(|app| async move {
+            if files::check_if_exists(app.app_base_dir(), "").await {
+                Some(app.clone())
+            } else {
+                None
+            }
+        })
+        .map(|app| app.clone())
         .collect()
+        .await
 }
 
 /// APP_INSTALLATIONS_VARIANTS contains the subset of variants of the App enum that are actual apps
