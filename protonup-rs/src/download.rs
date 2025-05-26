f0e4ed8a7a1287a3fa74e0ae2d99c2e0da040932
Byso <byso.dm@pm.me>
2025-05-26 19:23:11 +0200

    Making so that errors forwarded from functions actually print on error

--- protonup-rs/src/download.rs
+++ protonup-rs/src/download.rs
@@ -1,6 +1,5 @@
 use std::path::{Path, PathBuf};
-
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Result, anyhow, bail};
 use futures_util::stream::FuturesUnordered;
 use futures_util::{StreamExt, future, stream};
 use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle};
@@ -124,10 +123,7 @@ pub(crate) async fn validate_file(
     )
     .await?
     {
-        return Err(anyhow::Error::msg(format!(
-            "{} failed validation",
-            path.display()
-        )));
+        bail!("{} failed validation", path.display());
     }
 
     hash_progress_bar.set_style(get_message_bar_style().await);
@@ -317,24 +313,28 @@ pub async fn download_to_selected_app(app: Option<apps::App>) -> Result<Vec<Rele
 
     let multi_progress = MultiProgress::with_draw_target(ProgressDrawTarget::stderr_with_hz(20));
 
-    stream::iter(releases.clone())
-        .map(|release| {
-            let progress = multi_progress.clone();
-            let tool = selected_tool.clone();
-            let app_inst = app_inst.clone();
-            tokio::spawn(async move {
-                download_validate_unpack(release, app_inst, tool, progress).await
-            })
+    let tasks = releases.iter().map(|release| {
+        let release = release.clone();
+        let progress = multi_progress.clone();
+        let tool = selected_tool.clone();
+        let app_inst = app_inst.clone();
+        tokio::spawn(async move {
+            download_validate_unpack(release, app_inst, tool, progress).await
         })
-        .collect::<FuturesUnordered<_>>()
-        .await
-        .for_each(|res| {
-            if let Err(e) = res {
-                multi_progress.println(format!("{}", e)).unwrap();
-            }
-            future::ready(())
-        })
-        .await;
+    });
+
+    for task in tasks.collect::<FuturesUnordered<_>>().collect::<Vec<_>>().await {
+        let err: Option<anyhow::Error> = match task {
+            Ok(Ok(())) => None,
+            Ok(Err(e)) => Some(e),
+            Err(join_err) => Some(anyhow!(join_err)),
+        };
+
+        if let Some(e) = err {
+            eprintln!("{}", e);
+            return Err(anyhow!("Installation failed with Error"));
+        }
+    }
 
     Ok(releases)
 }
